20c4be0aaa71475782001a9011b0e72e09d3fa22
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-10-17 17:24:38 +0530

    Update carry_lookahead_adder_width.v

--- Carry Lookahead Adder.srcs/sources_1/new/carry_lookahead_adder_width.v
+++ Carry Lookahead Adder.srcs/sources_1/new/carry_lookahead_adder_width.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.10.2024 15:10:49
-// Design Name: 
-// Module Name: carry_lookahead_adder_width
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module carry_lookahead_adder
   #(parameter WIDTH = 8)
@@ -46,16 +27,13 @@ module carry_lookahead_adder
       end
   endgenerate
 
-  // Create the Generate (G) Terms:  Gi=Ai*Bi
-  // Create the Propagate Terms: Pi=Ai+Bi
-  // Create the Carry Terms:
   genvar j;
   generate
     for (j=0; j<WIDTH; j=j+1) 
       begin
-        assign wire_G[j]   = A[j] & B[j];
-        assign wire_P[j]   = A[j] | B[j];
-        assign wire_Carry[j+1] = wire_G[j] | (wire_P[j] & wire_Carry[j]);
+        assign wire_G[j]   = A[j] & B[j]; // Generate (G) Terms:  Gi=Ai*Bi
+        assign wire_P[j]   = A[j] | B[j]; // Generate (P) Terms:   Pi=Ai+Bi
+        assign wire_Carry[j+1] = wire_G[j] | (wire_P[j] & wire_Carry[j]); // Generate the equation of carry which is dependent on only the first value of the carry, that is wire_Carry[0] = 1'b0.
       end
   endgenerate
   
